e0930b6d2baf31ec051c231f5df2e8d4daf81d36
Lukáš Hlaváč <ccsio@protonmail.com>
2026-02-06 13:14:41 +0100

    removed prime-vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import PrimeVue from 'primevue/config';
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
@@ -10,6 +9,5 @@ const app = createApp(App)
 
 app.use(router)
 app.use(i18n)
-app.use(PrimeVue)
 
 app.mount('#app')
