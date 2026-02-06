48c9821bbe16a4617d342c1ff5f115dbea558697
Lukáš Hlaváč <ccsio@protonmail.com>
2026-02-06 15:05:40 +0100

    moved Navbar to App.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup>
 import SplashScreen from '../components/SplashScreen.vue'
-import Navbar from '../components/Navbar.vue'
 import StemRacing from '../components/StemRacing.vue'
 import Timeline from '../components/HomeTimeline.vue'
 </script>
 
 <template>
-  <Navbar />
   <SplashScreen />
   <StemRacing />
   <Timeline />  
